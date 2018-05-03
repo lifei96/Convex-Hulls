da3fd959a41758bed201a560cf759e73c12cab8c
lifei96 <lifei9696@gmail.com>
2018-05-03 17:19:28 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/Convex_Hulls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Convex_Hulls.dir/main.cpp.o"
+  "Convex_Hulls.pdb"
+  "Convex_Hulls"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Convex_Hulls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
